0e4e4018216338a46e6728389177dfe0f0bff411
rkmaurya577 <r.k.mauryadv@gmail.com>
2018-01-02 02:34:56 +0530

    added product_variation attribute

--- src/carts/models.py
+++ src/carts/models.py
@@ -1,16 +1,17 @@
 from django.db import models
 
 # Create your models here.
-from products.models import Product
+from products.models import Product, ProductVariation
 
 class CartItem(models.Model):
-	cart=models.ForeignKey('Cart',null=True,blank=True)
-	product=models.ForeignKey(Product)
+	cart = models.ForeignKey('Cart',null=True,blank=True)
+	product = models.ForeignKey(Product)
+	product_variations = models.ManyToManyField(ProductVariation,null=True,blank=True)
 	quantity = models.IntegerField(default=1)
-	line_total=models.DecimalField(default=10.99,max_digits=100,decimal_places=2)
+	line_total= models.DecimalField(default=10.99,max_digits=100,decimal_places=2)
 	notes = models.TextField(null=True,blank=True)
-	timestamp= models.DateTimeField(auto_now_add=True , auto_now=False)
-	updated= models.DateTimeField(auto_now_add=False , auto_now=True)
+	timestamp = models.DateTimeField(auto_now_add=True , auto_now=False)
+	updated = models.DateTimeField(auto_now_add=False , auto_now=True)
  
 	def __unicode__(self):
 		try:
