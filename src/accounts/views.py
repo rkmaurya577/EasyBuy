c0e20703d5235c5d586cda2b02cc6b5a0ab6b920
rkmaurya577 <r.k.mauryadv@gmail.com>
2018-01-05 15:00:00 +0530

    added registration_view

--- src/accounts/views.py
+++ src/accounts/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render,HttpResponseRedirect
 from django.contrib.auth import login, logout, authenticate
 # Create your views here.
-from .forms import LoginForm
+from .forms import LoginForm , RegistrationForm
 
 def logout_view(request):
 	logout(request)
@@ -18,5 +18,21 @@ def login_view(request):
 	context={
 		"form" : form
 	}
-	return render(request,"login.html",context)
+	return render(request,"form.html",context)
 
+
+def registration_view(request):
+	form = RegistrationForm(request.POST or None)
+	if form.is_valid():
+		print "valid"
+		new_user = form.save(commit=False)
+		# new_user.first_name = "rahul"  #here we can do other work
+		new_user.save()
+		# username=form.cleaned_data["username"]
+		# password=form.cleaned_data["password"]
+		# user=authenticate(username=username,password=password) #variable = parameter
+		# login(request,user)
+	context={
+		"form" : form
+	}
+	return render(request,"form.html",context)
