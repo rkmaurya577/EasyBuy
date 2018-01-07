73a927dabf41410b4b4cbe50e9391b484f302014
rkmaurya577 <r.k.mauryadv@gmail.com>
2018-01-08 00:53:40 +0530

    first commit

---
+++ src/marketing/middleware.py
@@ -0,0 +1,8 @@
+from .models import MarketingMessage
+
+class DisplayMarketing():
+	def process_request(self , request):
+		try:
+			request.session['marketing_message'] = MarketingMessage.objects.get_featured_item().message #call for custom queryset in .models
+		except:
+			request.session['marketing_message'] = False
