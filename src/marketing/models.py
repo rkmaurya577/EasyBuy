de4635087da32ebd10e940098bf82c4527de99a3
rkmaurya577 <r.k.mauryadv@gmail.com>
2018-01-08 00:57:35 +0530

    added MarketingManagerQuerySet and MarketingMessageManager

--- src/marketing/models.py
+++ src/marketing/models.py
@@ -1,6 +1,31 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+class MarketingManagerQuerySet(models.query.QuerySet):
+	def active(self):
+		return self.filter(active=True)
+
+	def featured(self):
+		return self.filter(featured = True)\
+					.filter(start_date__lt=timezone.now())\
+					.filter(end_date__gte=timezone.now())
+
+class MarketingMessageManager(models.Manager):
+	def get_queryset(self):
+		return MarketingManagerQuerySet(self.model,using=self._db)
+
+	def all(self):
+		return self.get_queryset().active()
+
+	def featured(self):
+		return self.get_queryset().active().featured()
+
+	def get_featured_item(self):
+		try:
+			return self.get_queryset().active().featured()[0]
+		except:
+			return None
 
 class MarketingMessage(models.Model):
 	message = models.CharField(max_length = 120)
@@ -11,5 +36,12 @@ class MarketingMessage(models.Model):
 	start_date = models.DateTimeField(auto_now_add=False , auto_now=False, null= True,blank=True)
 	end_date = models.DateTimeField(auto_now_add=False , auto_now=False, null= True,blank=True)
 
+	objects = MarketingMessageManager()
+	messages = MarketingMessageManager()
+
+	class Meta:
+		ordering = ["start_date" , "end_date"]
+
 	def __unicode__(self):
 		return str(self.message[:15])
+
