ffd7b6e42b1f6afcd85d7431bf817e95327a570d
rkmaurya577 <r.k.mauryadv@gmail.com>
2018-01-08 01:01:56 +0530

    added marketingmiddleware

--- src/easybuy/settings.py
+++ src/easybuy/settings.py
@@ -68,6 +68,7 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'marketing.middleware.DisplayMarketing',
 )
 
 ROOT_URLCONF = 'easybuy.urls'
