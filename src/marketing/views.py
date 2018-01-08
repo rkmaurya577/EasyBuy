7826c4ce29459711d1e2947acd4ff8d742babc10
rkmaurya577 <r.k.mauryadv@gmail.com>
2018-01-09 01:52:09 +0530

    added ajax for dismiss marketing message

--- src/marketing/views.py
+++ src/marketing/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+import json
+from django.shortcuts import render , HttpResponse
 
 # Create your views here.
+def dismiss_marketing_message(request):
+	if request.is_ajax():
+		data={"success":True}
+		print data
+		json_data = json.dumps(data)
+		print json_data
+		return HttpResponse(json_data, content_type="application/json")
