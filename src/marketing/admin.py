6e67d1c094b80086e530fc44578aabbd983b83c6
rkmaurya577 <r.k.mauryadv@gmail.com>
2018-01-11 16:46:04 +0530

    MarketingSliderAdmin added

--- src/marketing/admin.py
+++ src/marketing/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 
 # Register your models here.
 
-from .models import MarketingMessage
+from .models import MarketingMessage, MarketingSlider
 
 class MarketingMessageAdmin(admin.ModelAdmin):
 	list_display = ["__unicode__","start_date","end_date","active","featured"]
@@ -10,4 +10,11 @@ class MarketingMessageAdmin(admin.ModelAdmin):
 		model = MarketingMessage
 
 
+class MarketingSliderAdmin(admin.ModelAdmin):
+	list_display = ["__unicode__","order","start_date","end_date","active","featured"]
+	list_editable = ["order","start_date","end_date","active","featured"]
+	class Meta:
+		model = MarketingSlider
+
 admin.site.register(MarketingMessage , MarketingMessageAdmin)
+admin.site.register(MarketingSlider , MarketingSliderAdmin)
