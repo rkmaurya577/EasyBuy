3459c0adb39d97f62ed28d041b9f1c54397194de
rkmaurya577 <r.k.mauryadv@gmail.com>
2018-01-11 16:50:58 +0530

    added timezone

--- src/marketing/views.py
+++ src/marketing/views.py
@@ -1,11 +1,18 @@
 import json
-from django.shortcuts import render , HttpResponse
-
+import datetime
+from django.shortcuts import render , HttpResponse , Http404
+from django.utils import timezone
+from django.conf import settings
 # Create your views here.
 def dismiss_marketing_message(request):
 	if request.is_ajax():
 		data={"success":True}
 		print data
 		json_data = json.dumps(data)
+		request.session["dismiss_message_for"] = str(timezone.now()\
+			+ datetime.timedelta(hours=settings.MARKETING_HOURS_OFFSET \
+				,seconds=settings.MARKETING_SECONDS_OFFSET))
 		print json_data
 		return HttpResponse(json_data, content_type="application/json")
+	else:
+		raise Http404
