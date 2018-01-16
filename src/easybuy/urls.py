29acfb9f3afc3529bb3d71d930579c2b8ca3d528
rkmaurya577 <r.k.mauryadv@gmail.com>
2018-01-17 00:54:48 +0530

    added email_signup view

--- src/easybuy/urls.py
+++ src/easybuy/urls.py
@@ -16,7 +16,10 @@ urlpatterns = patterns('',
     url(r'^cart/(?P<slug>[\w-]+)/$',"carts.views.add_to_cart",name="add_to_cart"),
     url(r'^checkout/$',"orders.views.checkout",name="checkout"),
     url(r'^order/$',"orders.views.order",name="order"),
+    
     url(r'^ajax/dismiss_marketing_message$',"marketing.views.dismiss_marketing_message",name="dismiss_marketing_message"),
+    url(r'^ajax/email_signup$',"marketing.views.email_signup",name="email_signup"),
+
     url(r'^account/logout/$',"accounts.views.logout_view",name="logout"),
     url(r'^account/login/$',"accounts.views.login_view",name="login"),
     url(r'^account/register/$',"accounts.views.registration_view",name="registration"),
