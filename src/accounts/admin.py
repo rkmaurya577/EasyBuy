90bad5ba41b89881353e9f80d4d691c6be85d0c1
rkmaurya577 <r.k.mauryadv@gmail.com>
2018-01-19 00:43:11 +0530

    registered EmailMarketingSignUp and EmailMarketingSignupAdmin

--- src/accounts/admin.py
+++ src/accounts/admin.py
@@ -1,6 +1,14 @@
 from django.contrib import admin
 from .models import UserStripe, UserEmailConfirmed
+from accounts.models import EmailMarketingSignUp
 
 # Register your models here.
 admin.site.register(UserStripe)
 admin.site.register(UserEmailConfirmed)
+
+class EmailMarketingSignupAdmin(admin.ModelAdmin):
+	list_display = ("__unicode__","timestamp","updated")
+	class Meta:
+		model = EmailMarketingSignUp
+
+admin.site.register(EmailMarketingSignUp , EmailMarketingSignupAdmin)
