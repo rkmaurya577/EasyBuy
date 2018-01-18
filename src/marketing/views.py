f55970859dd54d585d656819c5bd172023ad9bda
rkmaurya577 <r.k.mauryadv@gmail.com>
2018-01-19 00:40:20 +0530

    added HttpResponseBadRequest and EmailMarketingSignUp

--- src/marketing/views.py
+++ src/marketing/views.py
@@ -1,19 +1,19 @@
 import json
 import datetime
 from django.shortcuts import render , HttpResponse , Http404
+from django.http import HttpResponseBadRequest
 from django.utils import timezone
 from django.conf import settings
 from .forms import EmailForm
+from accounts.models import EmailMarketingSignUp
 # Create your views here.
 def dismiss_marketing_message(request):
 	if request.is_ajax():
 		data={"success":True}
-		print data
 		json_data = json.dumps(data)
 		request.session["dismiss_message_for"] = str(timezone.now()\
 			+ datetime.timedelta(hours=settings.MARKETING_HOURS_OFFSET \
 				,seconds=settings.MARKETING_SECONDS_OFFSET))
-		print json_data
 		return HttpResponse(json_data, content_type="application/json")
 	else:
 		raise Http404
@@ -23,9 +23,11 @@ def email_signup(request):
 		form = EmailForm(request.POST or None)
 		if form.is_valid():
 			email = form.cleaned_data["email"]
+			EmailMarketingSignUp.objects.create(email=email)
+			request.session["marketing_email_added"] = True
 			return HttpResponse("success %s" %(email))
 		if form.errors:
 			json_data = json.dumps(form.errors)
-			return HttpResponse(json_data , content_type="application/json")
+			return HttpResponseBadRequest(json_data , content_type="application/json")
 	else:
 		raise Http404
