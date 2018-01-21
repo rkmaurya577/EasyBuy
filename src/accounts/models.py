8d6555e7740a32e18541dfec6ebf2e46b44c740b
rkmaurya577 <r.k.mauryadv@gmail.com>
2018-01-21 20:47:45 +0530

    addedl UserAddress model with state choices

--- src/accounts/models.py
+++ src/accounts/models.py
@@ -5,6 +5,35 @@ from django.core.urlresolvers import reverse
 # Create your models here.
 from django.template.loader import render_to_string
 
+STATE_CHOICES = (
+	('KA', 'Karnataka'), ('AP', 'Andhra Pradesh'), ('KL', 'Kerala'), ('TN', 'Tamil Nadu'), 
+	('MH', 'Maharashtra'), ('UP', 'Uttar Pradesh'), ('GA', 'Goa'), ('GJ', 'Gujarat'), ('RJ', 'Rajasthan'), 
+	('HP', 'Himachal Pradesh'), ('JK', 'Jammu and Kashmir'), ('TG', 'Telangana'), ('AR', 'Arunachal Pradesh'), 
+	('AS', 'Assam'), ('BR', 'Bihar'), ('CG', 'Chattisgarh'), ('HR', 'Haryana'), ('JH', 'Jharkhand'), 
+	('MP', 'Madhya Pradesh'), ('MN', 'Manipur'), ('ML', 'Meghalaya'), ('MZ', 'Mizoram'), ('NL', 'Nagaland'), 
+	('OR', 'Orissa'), ('PB', 'Punjab'), ('SK', 'Sikkim'), ('TR', 'Tripura'), ('UA', 'Uttarakhand'), 
+	('WB', 'West Bengal'), ('AN', 'Andaman and Nicobar'), ('CH', 'Chandigarh'), ('DN', 'Dadra and Nagar Haveli'), 
+	('DD', 'Daman and Diu'), ('DL', 'Delhi'), ('LD', 'Lakshadweep'), ('PY', 'Pondicherry'),
+	)
+
+
+class UserAddress(models.Model):
+	user = models.ForeignKey(settings.AUTH_USER_MODEL)
+	address = models.CharField(max_length=120)
+	address2 = models.CharField(max_length=120,null=True,blank=True)
+	city = models.CharField(max_length=120)
+	state = models.CharField(max_length=120, choices=STATE_CHOICES)
+	country = models.CharField(max_length=120)
+	zipcode = models.CharField(max_length=25)
+	phone = models.CharField(max_length=120)
+	shipping = models.BooleanField(default=True)
+	billing = models.BooleanField(default=False)
+	timestamp = models.DateTimeField(auto_now_add=True , auto_now=False)
+	updated = models.DateTimeField(auto_now_add=False , auto_now=True)
+
+	def __unicode__(self):
+		return str(self.user.username)
+
 
 class UserStripe(models.Model):
 	user= models.OneToOneField(settings.AUTH_USER_MODEL)
