82e86db052844d6dabd2c40c835f934c23d543c5
rkmaurya577 <r.k.mauryadv@gmail.com>
2018-01-21 20:42:15 +0530

    added UserAddressAdmin

--- src/accounts/admin.py
+++ src/accounts/admin.py
@@ -1,10 +1,16 @@
 from django.contrib import admin
-from .models import UserStripe, UserEmailConfirmed
+from .models import UserStripe, UserEmailConfirmed,UserAddress
 from accounts.models import EmailMarketingSignUp
 
+class UserAddressAdmin(admin.ModelAdmin):
+	class Meta:
+		model = UserAddress
+
+admin.site.register(UserAddress , UserAddressAdmin)
 # Register your models here.
 admin.site.register(UserStripe)
 admin.site.register(UserEmailConfirmed)
+
 
 class EmailMarketingSignupAdmin(admin.ModelAdmin):
 	list_display = ("__unicode__","timestamp","updated")
