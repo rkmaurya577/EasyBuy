128d7c6ac63f795b0ec678846733ffdb05b3fed7
rkmaurya577 <r.k.mauryadv@gmail.com>
2018-01-28 01:16:45 +0530

    added UserDeafultAddressAdmin

--- src/accounts/admin.py
+++ src/accounts/admin.py
@@ -1,11 +1,20 @@
 from django.contrib import admin
-from .models import UserStripe, UserEmailConfirmed,UserAddress
-from accounts.models import EmailMarketingSignUp
+from .models import UserStripe, UserEmailConfirmed,UserAddress,EmailMarketingSignUp,UserDefaultAddress
+
+
+class UserDefaultAddressAdmin(admin.ModelAdmin):
+	list_display = ["__unicode__","shipping","billing"]
+	list_editable = ["shipping","billing"]
+	class Meta:
+		model = UserDefaultAddress
 
 class UserAddressAdmin(admin.ModelAdmin):
+	list_display = ["__unicode__","address","shipping","billing"]
+	list_editable = ["address","shipping","billing"]
 	class Meta:
 		model = UserAddress
 
+admin.site.register(UserDefaultAddress , UserDefaultAddressAdmin)
 admin.site.register(UserAddress , UserAddressAdmin)
 # Register your models here.
 admin.site.register(UserStripe)
