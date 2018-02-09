218895076cf7710cab05a97dc097cb2b55e09925
Rahul Maurya <r.k.mauryadv@gmail.com>
2018-02-10 02:54:18 +0530

    generate stripe token and add & create card for payment

--- src/orders/views.py
+++ src/orders/views.py
@@ -1,7 +1,11 @@
 import time
+
+import stripe
+
 from django.shortcuts import render, HttpResponseRedirect
 from django.core.urlresolvers import reverse
 from django.contrib.auth.decorators import login_required
+from django.conf import settings
 # Create your views here.
 from carts.models import Cart
 from carts.models import Cart
@@ -9,6 +13,15 @@ from accounts.forms import UserAddressForm
 from accounts.models import UserAddress
 
 from .models import Order
+
+try:
+	stripe_pub = settings.STRIPE_PUBLISHABLE_KEY
+	stripe_sk= settings.STRIPE_SECRET_KEY
+except Exception ,e:
+	print "Exception : %s" %(e)
+	NotImplementedError(str(e))
+
+stripe.api_key = stripe_sk
 
 def order(request):
 	context={}
@@ -42,9 +55,33 @@ def checkout(request):
 	# 	new_address = address_form.save(commit = False)
 	# 	new_address.user = request.user
 	# 	new_address.save()
-	print "hii"
+	# print "hiiii"
 	current_addresses = UserAddress.objects.filter(user=request.user)
 	billing_addresses = UserAddress.objects.get_billing_addresses(user=request.user)
+
+	if request.method == "POST":
+		try:
+			user_stripe = request.user.userstripe.stripe_id
+			customer = stripe.Customer.retrieve(user_stripe)
+		except:
+			customer = None
+			pass	
+		if customer is not None:
+			token = request.POST['stripeToken']
+			print token
+			card = customer.sources.create(source=token)
+			charge = stripe.Charge.create(
+					amount = int(new_order.final_total * 100),
+					currency = "usd",
+					card = card,
+					customer = customer,
+					description = "Charge for %s" %(request.user.username)
+				)
+			print card
+			print charge
+			if charge['captured']:
+				print "charged"
+
 
 	#1add shipping address
 	#2add billing address
@@ -55,9 +92,12 @@ def checkout(request):
 		return HttpResponseRedirect(reverse("view_cart"))
 
 	context={ 
+		"order" : new_order,
 		"address_form" : address_form ,
 		"current_addresses" : current_addresses,
 		"billing_addresses" : billing_addresses,
+		"stripe_pub" : stripe_pub , 
+		"stripe_sk" : stripe_sk ,
 	}
 	template="orders/checkout.html"
 	return render(request,template,context)
